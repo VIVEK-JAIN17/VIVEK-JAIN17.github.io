71ea4956c04a076189cb50560bc40e108e15a3bd
VIVEK JAIN <60706386+VIVEK-JAIN17@users.noreply.github.com>
2020-07-07 15:17:06 +0530

    Update myPortfolio.js

--- JS/myPortfolio.js
+++ JS/myPortfolio.js
@@ -52,11 +52,12 @@ $(document).ready(function () {
             var hash = this.hash;
             $('html, body').animate({
 
-                scrollTop: ($(hash).offset().top - 30)
+                scrollTop: ($(hash).offset().top)
 
             }, 1000, function () {
 
                 window.location.hash = hash;
+                $(this).scrollTop($(hash).offset().top - 30);
 
             });
         }
